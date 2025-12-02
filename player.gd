ea5445a0dca4f0500ed46426127182a9cfed7993
MrWaddleman32 <ananth.v.vasishta@gmail.com>
2025-12-02 00:19:11 -0500

    Update player.gd

--- player.gd
+++ player.gd
@@ -4,10 +4,17 @@ extends CharacterBody3D
 var SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var coyote_time := 0.15
+var coyote_counter := 0.0
 @onready var dust_trail := $DustTrail
 
 func _physics_process(delta: float) -> void:
 	
+	if is_on_floor():
+		coyote_counter = coyote_time
+	else:
+		coyote_counter -= delta
+
 	var direction = Vector3()
 	if Input.is_action_pressed("move_left"):
 		direction.x = -1
@@ -28,15 +35,17 @@ func _physics_process(delta: float) -> void:
 	else:
 		dust_trail.emitting = false
 		SPEED = 5.0
-		
+	
 	
 	velocity.x = direction.x * SPEED
 	velocity.z = direction.z * SPEED
 	
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-	if is_on_floor() and Input.is_action_pressed("jump"):
+		
+	if Input.is_action_pressed("jump") and coyote_counter > 0:
 		velocity.y = JUMP_VELOCITY
+		coyote_counter = 0.0
 		
 	move_and_slide()
 	
