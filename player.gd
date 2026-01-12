709d7914ef8a495d99483c63ddd3afa11c9d92f5
MrWaddleman32 <ananth.v.vasishta@gmail.com>
2026-01-12 17:20:25 -0500

    idk what this did i ain gonna lie

--- player.gd
+++ player.gd
@@ -1,17 +1,27 @@
 extends CharacterBody3D
 
-
 var SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
 var coyote_time := 0.15
 var coyote_counter := 0.0
-var respawn_position = Vector3(0,1,0)
+
+var respawn_position = Vector3(0, 1, 0)
+
 @onready var dust_trail := $DustTrail
 @onready var anim_tree = $AnimationTree
 @onready var state_machine = anim_tree.get("parameters/playback")
 @onready var mesh = $MeshInstance3D
 @onready var collision_1 = $CollisionShape3D
+@export var mouse_sensitivity := 0.002
+@onready var camera = $Camera3D
+
+var pitch := 0.0
+
+func _unhandled_input(event):
+	# Removed mouse rotation entirely
+	pass
 
 func _physics_process(delta: float) -> void:
 	if is_on_floor():
@@ -20,10 +30,11 @@ func _physics_process(delta: float) -> void:
 		coyote_counter -= delta
 
 	var direction = Vector3()
-	var moving_up_right = Input.is_action_pressed("move_right") && Input.is_action_pressed("move_up")
-	var moving_up_left = Input.is_action_pressed("move_left") && Input.is_action_pressed("move_up")
-	var moving_down_right = Input.is_action_pressed("move_right") && Input.is_action_pressed("move_down")
-	var moving_down_left = Input.is_action_pressed("move_left") && Input.is_action_pressed("move_down")
+	var moving_up_right = Input.is_action_pressed("move_right") and Input.is_action_pressed("move_up")
+	var moving_up_left = Input.is_action_pressed("move_left") and Input.is_action_pressed("move_up")
+	var moving_down_right = Input.is_action_pressed("move_right") and Input.is_action_pressed("move_down")
+	var moving_down_left = Input.is_action_pressed("move_left") and Input.is_action_pressed("move_down")
+
 	if Input.is_action_pressed("move_left"):
 		direction.x = -1
 		mesh.rotation_degrees.y = 270
@@ -32,6 +43,7 @@ func _physics_process(delta: float) -> void:
 		direction.x = 1
 		mesh.rotation_degrees.y = 90
 		collision_1.rotation_degrees.y = 90
+
 	if Input.is_action_pressed("move_up"):
 		direction.z = -1
 		mesh.rotation_degrees.y = 180
@@ -40,6 +52,7 @@ func _physics_process(delta: float) -> void:
 		direction.z = 1
 		mesh.rotation_degrees.y = 0
 		collision_1.rotation_degrees.y = 0
+
 	if moving_up_right:
 		mesh.rotation_degrees.y = 135
 		collision_1.rotation_degrees.y = 135
@@ -52,16 +65,15 @@ func _physics_process(delta: float) -> void:
 	if moving_down_left:
 		mesh.rotation_degrees.y = 315
 		collision_1.rotation_degrees.y = 315
-	
+
 	var horizontal_velocity = Vector3(velocity.x, 0, velocity.z)
 	var moving = horizontal_velocity.length() > 0.1
 	anim_tree.set("parameters/conditions/is_moving", moving)
 	anim_tree.set("parameters/conditions/is_idle", !moving)
-	
+
 	direction = direction.normalized()
-	
-	
-	if Input.is_action_pressed("sprint") && moving:
+
+	if Input.is_action_pressed("sprint") and moving:
 		dust_trail.emitting = true
 		SPEED = 7.5
 		if not is_on_floor():
@@ -69,38 +81,36 @@ func _physics_process(delta: float) -> void:
 	else:
 		dust_trail.emitting = false
 		SPEED = 5.0
-	
+
 	if global_transform.origin.y < -10:
 		global_transform.origin = respawn_position
 		velocity = Vector3.ZERO
-	
-	
+
 	velocity.x = direction.x * SPEED
 	velocity.z = direction.z * SPEED
-	
+
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-		
+
 	if Input.is_action_pressed("jump") and coyote_counter > 0:
 		velocity.y = JUMP_VELOCITY
 		coyote_counter = 0.0
-		
+
 	move_and_slide()
-	
-	
 
-
+# -----------------------
+# Teleports
+# -----------------------
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body == self:
 		call_deferred("_teleport_to_engineering")
 
 func _teleport_to_engineering():
 	get_tree().change_scene_to_file("res://engineering_world.tscn")
-	
+
 func _teleport_to_main():
 	get_tree().change_scene_to_file("res://floor.tscn")
 
-
 func _on_tp_to_main_body_entered(body: Node3D) -> void:
 	if body == self:
-		call_deferred("_teleport_to_main") # Replace with function body.
+		call_deferred("_teleport_to_main")
