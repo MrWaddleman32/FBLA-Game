f4be56f7c8899a3c50bcf9e3711675727824f515
MrWaddleman32 <ananth.v.vasishta@gmail.com>
2026-03-09 13:21:07 -0400

    extra things

--- science_world.gd
+++ science_world.gd
@@ -1,19 +1,37 @@
 extends Node3D
 
 @onready var player: CharacterBody3D = $Player
-
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+var in_lab_station = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	await get_tree().process_frame
+	Inventory.inventory.clear()
 	player.can_move = false
+	CutsceneManager.turn_off_UI()
+	await CutsceneManager.wait(2)
+	CutsceneManager.turn_on_UI()
 	CutsceneManager.change_text("Finally we will be learning about a day in the life of a scientist")
-	CutsceneManager.wait(3)
+	await CutsceneManager.wait(3)
 	CutsceneManager.change_text("That door at the end is locked and the only way to open it\
-	is to explode it open using chemical reactions")
-	CutsceneManager.wait(3)
+	 is to explode it open using chemical reactions")
+	await CutsceneManager.wait(3)
+	CutsceneManager.change_text("Follow me")
+	await CutsceneManager.wait(2)
+	player.can_move = true
+	animation_player.play("Follow path")
 	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if in_lab_station and Input.is_action_just_pressed("pick_up_item"):
+		open_mixing_station()
+
+
+func _on_lab_station_body_entered(body: Node3D) -> void:
+	if body == player:
+		in_lab_station = true
+
+func open_mixing_station():
 	pass
